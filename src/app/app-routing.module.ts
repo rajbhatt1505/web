c604ea387ab0c4d1d8bcffc38c8d888ad72b3e20
rajbhatt1505 <rb794095@gmail.com>
2022-11-22 11:29:56 +0530

    web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ {path:'courses',component:CoursesComponent},
+ {path:'about',component:AboutComponent},
+ {path:'team',component:TeamComponent},
+ {path:'pricing',component:PricingComponent},
+ {path:'blog',component:BlogComponent},
+ {path:'contact',component:ContactComponent},
+ {path:'**', component:HomeComponent},
+ 
+//  {path:'home/home.component.html',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    AboutComponent,
+    TeamComponent,
+    PricingComponent,
+    BlogComponent,
+    ContactComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
